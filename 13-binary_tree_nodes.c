2a6ff50a54f6509c197b78a787c54006d63c8a3d
SafieDeme9 <safiedeme9@gmail.com>
2023-03-01 17:22:02 -0500

    task 13 test using counter fixed declaration

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,13 +9,14 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
 	size_t count = 0;
 
+	if (tree == NULL)
+		return (0);
 	if (tree->left != NULL || tree->right != NULL)
 		count++;
 	count += binary_tree_nodes(tree->left);
 	count += binary_tree_nodes(tree->right);
+
 	return (count);
 }
