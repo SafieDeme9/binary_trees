d05c375a21c754d50621838f45fcbc42602605e6
SafieDeme9 <safiedeme9@gmail.com>
2023-03-01 16:59:00 -0500

    task 13 testing without binary_tree_size and binary_tree_leaves in the 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,36 +1,6 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - counts the leaves in a binary tree
- * @tree: pointer to the root node of the tree to count
- *
- * Return: 0 if tree is NULL, number of leaves.
- */
-
-size_t binary_tree_leaves(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	return ((!tree->left && !tree->right) ? 1 :
-			binary_tree_leaves(tree->left) +
-			binary_tree_leaves(tree->right));
-}
-
-/**
- * binary_tree_size - measures the size of a binary tree
- * @tree: pointer to the root node of the tree to measure the size
- * Return: size or 0 if tree is NULL
- */
-
-size_t binary_tree_size(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
-}
-
-/**
  * binary_tree_nodes - counts the nodes with at least one child
  * @tree: pointer to the root node of the tree to count numbers of nodes
  *
